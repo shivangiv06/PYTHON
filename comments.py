a0127399937d8ea90b037ed8778d03c5d942d3a0
Shivangi Verma <shivangiverma020@bbdu.ac.in>
2024-05-09 01:55:56 +0530

    Adding new files

---
+++ comments.py
@@ -0,0 +1,25 @@
+print("Hey, I am Shivangi Verma. \nCurrently persuing MCA")
+#In this we use \n to make a new line
+#escape sequence character =\n
+
+
+# Ctrl+FOrward slash [Ctrl+/] shortcut for making comments
+# of multiple lines
+
+#=is called Pound symbol.
+
+#writing multiple line comment
+'''this is comment
+this i s mulyiline cooment
+triple single quote or triple double quote is used to
+comment and uncomment the lines.'''
+
+print("HEy , I am a \"good girl\".\n and who see my code is also a good boy/girl")
+
+#to join the different input we use sep
+
+print("Hey",6,7, sep="~",end="009")
+print("harry")
+
+print("Hey",6,7, sep="~",end="009\n")
+print("harry")
---
+++ demo.py
@@ -0,0 +1,7 @@
+print("Hello world")
+print(5)
+print(8+9)
+print(8*9)
+print(4/2)
+print(5%2)
+print("My name is Shivangi Verma.")
