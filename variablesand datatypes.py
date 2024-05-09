020c0de43d717632a77e7c046ec6a45b349a6288
Shivangi Verma <shivangiverma020@bbdu.ac.in>
2024-05-09 20:15:56 +0530

    adding new file variablesanddataypes

---
+++ variablesand datatypes.py
@@ -0,0 +1,22 @@
+#It is about datatypes and variables.
+#variables are the containers.
+
+a=1
+print(a)
+
+b="Shivi"
+print(b) 
+
+a=1
+b="Shiv"
+c=True
+d = None
+e= 2.7
+f= complex(8,3)
+
+print(type(a)) #It defines the type of variable.print(type(variable name))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
